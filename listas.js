c16542356a169cfe1a522bbc19779be3b7d881bc
SoyGersson07 <gerssonvalenciasilva@gmail.com>
2026-03-16 20:06:46 -0500

    subir archivos

--- listas.js
+++ listas.js
@@ -2,3 +2,13 @@
 console.log("Listas, pilas y colas");
 console.log("Listas, pilas y colas");
 console.log("Listas, pilas y colas");
+
+// Listas
+let lista = [1, 2, 3, 4, 5];
+
+lista.push(6); // Agrega un elemento al final de la lista
+lista.unshift(0); // Agrega un elemento al inicio de la lista
+lista.pop(); // Elimina el último elemento de la lista
+lista.shift(); // Elimina el primer elemento de la lista    
+
+console.log(lista); // Imprime la lista actualizada
